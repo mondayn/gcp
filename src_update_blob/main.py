8b41bc7a2c17bb55b04602106d83cd8cb4e0136b
mondayn <nathan.monday@gmail.com>
2024-12-16 15:03:48 -0600

    updated projectid

--- src_update_blob/main.py
+++ src_update_blob/main.py
@@ -5,6 +5,6 @@ import datetime
 @functions_framework.cloud_event
 def update_bucket(cloud_event):
     s = datetime.datetime.now().strftime("%m-%d-%y %H:%M:%S")
-    PROJECT_ID='hca-hvu7470-202411221829-sbx'
-    bucket = storage.Client(PROJECT_ID).bucket('hca-hvu7470-202411221829-sbx_b1')
+    PROJECT_ID=storage.Client().project
+    bucket = storage.Client(PROJECT_ID).bucket(f'{PROJECT_ID}_b1')
     bucket.blob('test.txt').upload_from_string(s)
